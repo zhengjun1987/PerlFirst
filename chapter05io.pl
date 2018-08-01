19e23f53e72eb4c194fd9c4b05e123452359d7cf
郑军 <zhengjun1987@outlook.com>
2018-08-01 23:56:41 +0800

    chapter05 perl I/O

---
+++ chapter05io.pl
@@ -0,0 +1,47 @@
+#! /usr/bin/perl
+use 5.012;
+
+# 	my $input = undef;
+# 	while(defined($input = <STDIN>)){
+# 		print "I see $input";
+# 	}
+# 
+# 			192:Desktop zhengjun$ ./chapter05io.pl 
+# 			是谁，在敲打我 窗
+# 			I see 是谁，在敲打我窗
+# 
+# 			是谁，在撩动 琴弦
+# 			I see 是谁，在撩琴弦
+# 
+# 			^Z
+# 			[1]+  Stopped                 ./chapter05io.pl
+
+# 	while (defined($_ = <STDIN>)){
+# 		print "I saw $_";
+# 	}
+# 			192:Desktop zhengjun$ ./chapter05io.pl 
+# 			2018年08月01日23:44:54
+# 			I saw 2018年08月01日23:44:54
+# 			请允许我尘埃落定
+# 			I saw 请允许我尘埃落定
+# 			用沉默埋葬了过去
+# 			I saw 用沉默埋葬了过去
+# 			千言万语只能无语
+# 			I saw 千言万语只能无语
+# 			为什么我用尽全身力气只换来半生回忆
+# 			I saw 为什么我用尽全身力气只换来半生回忆
+# 			原来你也在这里
+# 			I saw 原来你也在这里
+# 
+# 			I saw 
+# 			^Z
+# 			[3]+  Stopped                 ./chapter05io.pl
+
+# 	foreach (@_){
+# 		print;
+# 	}
+# 	say "program end."
+# 			192:Desktop zhengjun$ ./chapter05io.pl 
+# 			program end.
+# Perl的foreach所针对的数组可以为空，不像Java
+
