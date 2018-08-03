e820ca1b5450c6bd5ab01e64649c9ca05e2b0efd
郑军 <zhengjun1987@outlook.com>
2018-08-03 23:39:04 +0800

    chapter05 formatted output

--- chapter05io.pl
+++ chapter05io.pl
@@ -45,3 +45,50 @@ use 5.012;
 # 			program end.
 # Perl的foreach所针对的数组可以为空，不像Java
 
+# 	@ARGV = qw(/larry moe culy/);
+# 	while (defined ($_ = <>)){
+# 		print;
+# 	}
+
+# 	my $name = "Larry Wall";
+# 	print "Hello there, $name ,did you know that 3+4 is ",3+4,"?\n";
+# 			192:PerlFirst zhengjun$ ./chapter05io.pl chapter05io.pl 
+# 			Hello there, Larry Wall ,did you know that 3+4 is 7?
+
+# 	my @arr = (1,2,4,5,6);
+# 	say "@arr";
+# 			192:PerlFirst zhengjun$ ./chapter05io.pl chapter05io.pl 
+# 			1 2 4 5 6
+
+# 	my ($user,$days_to_expire) = ("Merlyn",3);
+# 	printf "Hello,%s; your password expires in %f days!\n",$user,$days_to_expire;
+# 				================================================================================
+# 				2018年8月3日 23:12:28
+# 				~/Desktop/PerlFirst/chapter05io.pl
+# 				--------------------------------------------------------------------------------
+# 				Hello,Merlyn; your password expires in 3.000000 days!
+
+# 	printf "%g %g %g\n",2**64,5/2,51/17;
+# 					1.84467e+19 2.5 3
+
+# 	printf "%.3g",2**64;
+# 				1.84e+19
+
+# 	printf "Monthly interest rate:%.2f%%",5.25/12;
+# 			================================================================================
+# 			2018年8月3日 23:20:00
+# 			~/Desktop/PerlFirst/chapter05io.pl
+# 			--------------------------------------------------------------------------------
+# 			Monthly interest rate:0.44%
+
+# 	my @items = qw/wilma dino pebble/;
+# 	my $format = "The items are :\n".("%10s\n"x@items);
+# 	printf $format,@items;
+# 			================================================================================
+# 			2018年8月3日 23:36:22
+# 			~/Desktop/PerlFirst/chapter05io.pl
+# 			--------------------------------------------------------------------------------
+# 			The items are :
+# 				 wilma
+# 				  dino
+# 				pebble
